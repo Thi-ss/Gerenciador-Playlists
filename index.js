5db087d3a19d46c9623f6948b9e84eb011fab351
Thi-ss <thiago.ssilva2215@gmail.com>
2026-09-10 09:01:30 -0300

    feat: criação

--- index.js
+++ index.js
@@ -15,20 +15,7 @@ let playlist = [
     "Paranoid"
 ];
 
-console.log(`
-    ======Menu======
-    1 - Pesquisar música
-    2 - Criar seleção
-    3 - Remover Música
-    4 - Inserir música
-    5 - Ordenar A-Z
-    6 - Inverter playlist
-    7 - Exibir playlist
-    8 - Sair`);
-
 let opcao = ""
-opcao = prompt(`Escolha uma opção: `)
-
 while (opcao ==! "0"){
     console.log(`
         ---Menu---
@@ -40,14 +27,16 @@ while (opcao ==! "0"){
         6 - Inverter playlist
         7 - Exibir playlist
         8 - Sair`)
+
+opcao = prompt(`Escolha uma opção: `)
 }
 
 
 switch (opcao) {
-    case "1":
-        let pesquisarMusica = prompt((`Pesquisar música na playlist: ${playlist.join(`, `)}`));
-
-    if (playlist.includes(pesquisarMusica)){
+case "1":
+    let pesquisarMusica = prompt((`Pesquisar música na playlist: ${playlist.join(`, `)}`));
+    
+    if (playlist.includes(pesquisarMusica)) {
         console.log(`${pesquisarMusica} encontrada!`);
         console.log(`${pesquisarMusica} encontrada!, posição: ${playlist.indexOf(pesquisarMusica)}`);
     } else {
@@ -55,11 +44,25 @@ switch (opcao) {
         console.log(`${pesquisarMusica} não encontrada!`);
     }
 
-    case "2":
-        console.log(`Selecione as músicas que deseja ver da playlist (sequência númerica das músicas: 0, 1, 2...): ${playlist.join(`, `)}`);
-        console.log(`Para selecionar uma fatia das músicas, digite a posição da música, e quantas apartir dela deseja reunir.`);
+case "2":
+    console.log(`Selecione as músicas que deseja ver da playlist (sequência númerica das músicas: 0, 1, 2...): ${playlist.join(`, `)}`);
+    console.log(`Para selecionar uma fatia das músicas, digite a posição da música, e quantas apartir dela deseja reunir.`);
+    let posicaoSlice = prompt((`Digite posição: `));
+    let quantiaSlice = prompt((`Digite quantia: `));
+    let recortePlaylist = playlist.slice(posicaoSlice, quantiaSlice);
+    console.log(`${recortePlaylist}`);
 
-        let escolhaSlice = prompt((`Digite na seguinte ordem: posição e quantia:`))
-        let recortePlaylist = playlist.slice(a, b);
+case "3":
+    let remover = prompt((`Selecione uma música que deseja remover.`))
+
+    if (playlist.includes(remover)) {
+        let indice = playlist.indexOf(remover);
+        playlist.splice(indice, 1);
+        console.log(`Música: ${remover} removida.`);
+    } else {
+        console.log(`Essa música não pertence à playlist.`);
+    }
+
+case "4":
     
 }
